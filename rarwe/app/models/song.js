bcac4f7c9455909b5433223356b0c390cae1b0dc
Franco Borrelli <francoborrelligobertti@gmail>
2022-07-05 11:05:42 -0300

    Fetch songs and bands

--- rarwe/app/models/song.js
+++ rarwe/app/models/song.js
@@ -1,7 +1,8 @@
 export default class Song{
-  constructor({title, rating, band}){
+  constructor({title, rating, band}, relationships = {}){
     this.title = title;
     this.rating = rating ?? 0;
     this.band = band;
+    this.relationships=relationships;
   }
 }
