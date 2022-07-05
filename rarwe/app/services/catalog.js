66ed5a342ce5a8543d00bae9977c1634feca310c
Franco Borrelli <francoborrelligobertti@gmail>
2022-07-05 13:35:00 -0300

    Before identity map

--- rarwe/app/services/catalog.js
+++ rarwe/app/services/catalog.js
@@ -1,31 +1,151 @@
 import Service from '@ember/service';
-import { tracked } from 'tracked-built-ins'; 
+import Band from 'rarwe/models/band';
+import { tracked } from 'tracked-built-ins';
+import Song from 'rarwe/models/song';
+import { isArray } from '@ember/array';
+
+//#extract the relationships from an item from JSON
+function extractRelationships(object) {
+  let relationships = {};
+  for (let relationshipName in object) {
+    relationships[relationshipName] =
+      object[relationshipName].links.related;
+  }
+  return relationships;
+}
+
+
 
 
 export default class CatalogService extends Service {
-storage = {};
+  storage = {};
 
-constructor(){
-  super(...arguments);
-  this.storage.bands = tracked([]);
-  this.storage.songs = tracked([]);
-}
+  constructor() {
+    super(...arguments);
+    this.storage.bands = tracked([]);
+    this.storage.songs = tracked([]);
+  }
 
-add(type, record){
-  let collection = type === 'band' ? this.storage.bands : this.storage.songs;
-  collection.push(record);
-}
 
-get bands(){ 
-  return this.storage.bands;
-}
+  //#--- Fetch's alll
+  async fetchAll(type) {
+    if (type === 'bands') {
+      let response = await fetch('/bands');
+      let json = await response.json();
+      this.loadAll(json);
+      return this.bands;
+    }
 
-get songs(){
-  return this.storage.songs; 
-}
+    if (type === 'songs') {
+      let response = await fetch('/songs');
+      let json = await response.json();
+      this.loadAll(json);
+      return this.songs;
+    }
 
-find(type, filterFn){
-  let collection = type === 'band' ? this.bands : this.songs;
-  return collection.find(filterFn);
-}
+  }
+
+
+  loadAll(json) {
+    let records = [];
+    for (let item of json.data) {
+      records.push(this._loadResource(item));
+    }
+    return records;
+  }
+
+
+  _loadResource(data) {
+    let record;
+    let { id, type, attributes, relationships } = data;
+    if (type === 'bands') {
+      let rels = extractRelationships(relationships);
+      record = new Band({ id, ...attributes }, rels);
+      this.add('band', record);
+    }
+    if (type === 'songs') {
+      let rels = extractRelationships(relationships);
+      record = new Song({ id, ...attributes }, rels);
+      this.add('song', record);
+    }
+    return record;
+  }
+
+  load(response) {
+    return this._loadResource(response.data);
+  }
+
+  //#create the JSON for a band or a song
+  async create(type, attributes, relationships = {}) {
+    let payload = {
+      data: {
+        type: type === 'band' ? 'bands' : 'songs',
+        attributes,
+        relationships,
+      },
+    };
+    let response = await fetch(type === 'band' ? '/bands' : '/songs', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/vnd.api+json',
+      },
+      body: JSON.stringify(payload),
+    });
+    let json = await response.json();
+    return this.load(json);
+  }
+
+  //#--------------------------
+
+  //#---Update
+  async update(type, record, attributes) {
+    let payload = {
+      data: {
+        id: record.id,
+        type: type === 'band' ? 'bands' : 'songs',
+        attributes,
+      },
+    };
+    let url = type === 'band' ? `/bands/${record.id}` :
+      `/songs/${record.id}`;
+    await fetch(url, {
+      method: 'PATCH', headers: {
+        'Content-Type': 'application/vnd.api+json',
+      },
+      body: JSON.stringify(payload),
+    });
+  }
+
+  add(type, record) {
+    let collection = type === 'band' ? this.storage.bands : this.storage.songs;
+    collection.push(record);
+  }
+
+  //# fetch songs and band
+  async fetchRelated(record, relationship) {
+    let url = record.relationships[relationship];
+    let response = await fetch(url);
+    let json = await response.json();
+    if (isArray(json.data)) {
+      record[relationship] = this.loadAll(json);
+    }
+    else {
+      record[relationship] = this.load(json);
+    }
+    return record[relationship];
+  }
+
+  // #-------- Getters, Setters and Find
+  get bands() {
+    return this.storage.bands;
+  }
+
+  get songs() {
+    return this.storage.songs;
+  }
+
+  find(type, filterFn) {
+    let collection = type === 'band' ? this.bands : this.songs;
+    return collection.find(filterFn);
+  }
 }
